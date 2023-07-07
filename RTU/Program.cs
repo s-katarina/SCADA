92bfdb2edb7e444eebac2930ec521c38f4977ca9
miloss01 <mikicamiki.bat@gmail.com>
2023-07-07 06:50:31 +0200

    Add RTU project

---
+++ RTU/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RTU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
